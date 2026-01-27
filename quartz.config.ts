0b0e26d5fcfe89a9be7b526b9fc3f0cb54618f12
sunshinetoyou <yeonsu@dankook.ac.kr>
2026-01-27 13:26:31 +0900

    FIX: Change BaseURL to https://sunshinetoyou.github.io/danyeon-blog/

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "sunshinetoyou.github.io/danyeon-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
